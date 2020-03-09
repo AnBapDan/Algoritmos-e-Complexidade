2d984ab83e01b46326b6f7d25e3adf3ff1fe6e4d
Daniel <dani.andrade@ua.pt>
2020-03-09 19:51:25 +0000

    Optimized version Ex2

--- Guiao3/Ex22.c
+++ Guiao3/Ex22.c
@@ -1,35 +1,58 @@
 #include<stdio.h>
 
 unsigned int size = 0 ;
-
+unsigned int index = 0;
+unsigned int iteracoes = 0;
 int counter(int array[]){
-  int indexval = 0;
-  int amnt = 0;
-  int i= size-9;
-  for(int i; i>0; i--){                               //indice
+  iteracoes = 0;
+  index = 0;
+  int amnt = -1;
+  int i = size-1;
+  for(i; i>0; i--){                                   //indice
     int count = 0;
     for(int j =0; j< i; j++){                         //valores
+
       if(amnt > i){                                   // Se a qntdd de numeros menores de um indice for maior que o indice a verificar, podemos sair
-        return amnt;
-        return indexval;
+        return index;
       }
+      iteracoes++;
       if(array[j]<array[i]){
         count++;
       }
+    }
+    if(count >= amnt){
+      amnt = count;
+      index = i;
+      printf("%d \n", amnt);
     }
 
   }
   if(amnt == 0){
     return -1;
   }
+
+  return 0;
 }
 
 
 int main(void){
 
-  array[10] = {1, 9, 2, 8, 3, 4, 5, 3, 7, 2};
-  array1[10] ={1, 7, 4, 6, 5, 2, 3, 2, 1, 0};
-  array2[10] ={2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+  int array[10] = {1, 9, 2, 8, 3, 4, 5, 3, 7, 2};
+  size = sizeof(array)/sizeof(int);
+  int x = counter(array);
+  printf("Elemento na posição: %d   Nmr de iteracoes: %d \n",x, iteracoes);
+
+  int array1[10] ={1, 7, 4, 6, 5, 2, 3, 2, 1, 0};
+  iteracoes = 0;
+  size = sizeof(array)/sizeof(int);
+  x = counter(array1);
+  printf("Elemento na posição: %d   Nmr de iteracoes: %d \n",x, iteracoes);
+
+  int array2[10] ={2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+  iteracoes = 0;
+  size = sizeof(array)/sizeof(int);
+  x = counter(array2);
+  printf("Elemento na posição: %d   Nmr de iteracoes: %d \n",x, iteracoes);
 
   return 0;
 
